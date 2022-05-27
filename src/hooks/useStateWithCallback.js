8c721fcdb2c3f3e8a9534f1c59177600d0efb8b4
Sarthak Shukla <sarthakshukla1316@gmail.com>
2022-05-27 17:17:34 +0530

    Removed unnecessary code

--- src/hooks/useStateWithCallback.js
+++ src/hooks/useStateWithCallback.js
@@ -1,36 +1,3 @@
-// import { useCallback, useEffect, useRef, useState } from "react"
-
-// export const useStateWithCallback = (initialState) => {
-//     const [state, setState] = useState(initialState);
-
-//     const cbRef = useRef(null);
-//     const updateState = useCallback(( newState, cb ) => {
-//         cbRef.current = cb;
-        
-//         setState((prev) => {
-//             return typeof newState === 'function' ? newState(prev) : newState;
-//         })
-//     }, []);
-
-//     useEffect(() => {
-//         if(cbRef.current) {
-//             cbRef.current(state);
-//             cbRef.current = null;
-//         }
-//     }, [state]);
-    
-    
-//     return [state, updateState];
-// }
-
-
-
-
-
-
-
-
-
 import { useState, useRef, useEffect, useCallback } from 'react';
 export const useStateWithCallback = (intialState) => {
     const [state, setState] = useState(intialState);
